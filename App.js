27a723cce899ea253827e292cf25118e77a24a39
Sithija Shehara <nsithijashehara@gmail.com>
2019-12-25 09:49:37 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+//components
+import ChatScreen from './components/ChatScreen';
+import LoginScreen from './components/LoginScreen';
+
+const AppNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Chat: ChatScreen
+  },
+  {
+    headerMode:'none'
+  }
+); 
+
+export default AppContainer = createAppContainer(AppNavigator);
+
