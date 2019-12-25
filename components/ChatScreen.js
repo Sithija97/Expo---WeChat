389f1aed6cd0d1b3906a65fd3a418556a9b6c791
Sithija Shehara <nsithijashehara@gmail.com>
2019-12-25 11:35:19 +0530

    added chat icone and done basic stylen on login

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function ChatScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Chat Screen</Text>
-    </View>
-  );
+export default class ChatScreen extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>Chat Screen</Text>
+            </View>
+        );
+    }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
-  );
+export default class LoginScreen extends React.Component{
+    state={
+        name:""
+    };
+
+    continue = ()=> {
+        this.props.navigation.navigate("Chat",{name:this.state.name});
+    };
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <View style={styles.circle}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F4F5F7',
   },
+  circle: {
+      height:500,
+      width:500,
+      position:'absolute',
+      backgroundColor:'#FFF',
+      borderRadius: 500 / 2,
+      left:-120,
+      top:-20
+  }
 });
