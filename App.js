f3f483392e8138edbf7a24c296b5a9064ee37bcc
Sithija Shehara <nsithijashehara@gmail.com>
2019-12-27 23:29:23 +0530

    error in chat -> refOn

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 //components
+import Login from './components/Login';
+import Signup from './components/SignUp';
 import Chat from './components/Chat';
 
 const AppNavigator = createStackNavigator(
   {
+    Login:Login,
+    Signup:Signup,
     Chat: Chat
   },
   {
--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat"; // 0.3.0
+
+import firebaseSDK from "../config/firebaseSDK";
 
 export default class Chat extends React.Component {
+  componentDidMount() {
+    firebaseSDK.refOn(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message)
+      }))
+    );
+  }
+  componentWillUnmount() {
+    firebaseSDK.refOff();
+  }
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || "Chat!"
+  });
+
+  state = {
+    messages: []
+  };
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      email: this.props.navigation.state.params.email,
+      avatar: this.props.navigation.state.params.avatar,
+      id: firebaseSDK.uid,
+      _id: firebaseSDK.uid
+    };
+  }
+
   render() {
-    return <GiftedChat />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={firebaseSDK.send}
+        user={this.user}
+      />
+    );
   }
 }
